f6a986a5db134990eb423ed46359f03857d9537b
Mahmoud Lahham <mahlahsy@yahoo.com>
2022-07-03 16:23:09 +0200

    add page

--- src/App.js
+++ src/App.js
@@ -2,18 +2,10 @@
 
 import React from "react";
 import "./App.css";
-import { Divider } from "@mui/material";
-import Header from "./components/Header";
-import IconBar from "./components/IconBar";
+import Home from "./pages/Home";
 
 const App = () => {
-  return (
-    <div>
-      <Header />
-      <Divider />
-      <IconBar />
-    </div>
-  );
+  return <Home />;
 };
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+/** @format */
+
+import React from "react";
+import { Divider } from "@mui/material";
+import Header from "../components/Header";
+import IconBar from "../components/IconBar";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Divider />
+      <IconBar />
+    </>
+  );
+};
+
+export default Home;
